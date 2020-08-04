34bacf68ab8025ca50f61b622c6816c289998522
Student User <student@workstation.lab.example.com>
2020-08-04 04:10:08 +0000

    Changed index page contents.

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
    <title>PHP TEST</title>
     </head>
     <body>
-    <?php echo "<p>Hello World</p>"; 
-     ?>
+    <?php
+      print " Hello World php version is " . PHP_VERISON . "\n"; 
+      print "A change is commit!\n"; 
+    ?>
     </body>
     </html>
